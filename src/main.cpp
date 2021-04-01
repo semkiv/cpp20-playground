30429a80902f5fe6be4517e739cdedcd2628d59a
Andrii Semkiv <semkiv@gmail.com>
2021-04-01 23:17:41 +0300

    [General] Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// import <cstdlib>;
+// import <iostream>;
+
+#include <cstdlib>
+#include <iostream>
+
+int main([[maybe_unused]] int const argc, [[maybe_unused]] char const * const argv[])
+{
+    std::cout << "Hello C++20 world!" << std::endl;
+
+    return EXIT_SUCCESS;
+}

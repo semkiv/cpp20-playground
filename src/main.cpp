f8827048df354fbb27a9267a861d5505412856da
Andrii Semkiv <semkiv@gmail.com>
2021-04-10 18:59:11 +0300

    [General] Better project.
    Added config files for `clang-format` and `clang-tidy`.
    Added `clang-tidy` and `iwyu` as optional build steps.
    Formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,20 @@
 #include <cstdlib>
 #include <iostream>
 #include <thread>
+#include <utility>
 
-auto main([[maybe_unused]] int const argc, [[maybe_unused]] char const * const argv[]) -> int
-{
-    auto jt = std::jthread{
-        [] { std::cout << "Hello C++20 world!" << std::endl; }
-    };
+class Greeter {
+public:
+    template<typename Function, typename... Args>
+    Greeter(Function && f, Args &&... args)
+            : m_worker{std::forward<Function>(f), std::forward<Args>(args)...} {}
+
+private:
+    std::jthread m_worker;
+};
+
+auto main([[maybe_unused]] int const argc, [[maybe_unused]] char const * const argv[]) -> int {
+    auto const greeter = Greeter{[] { std::cout << "Hello C++20 world!" << std::endl; }};
 
     return EXIT_SUCCESS;
 }
